cf9460e317fa2f796a1ea0ba7925735d155b0161
leonardo <leoruizz14@outlook.com>
2024-04-03 12:11:02 -0300

    service changes

--- lib/bytebank_balance.dart
+++ lib/bytebank_balance.dart
@@ -6,8 +6,3 @@ import 'widgets/type_pin_dialog.dart';
 
 part 'widgets/balance_widget.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
--- lib/service/balance_service.dart
+++ lib/service/balance_service.dart
@@ -3,11 +3,11 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class BalanceService {
-  String url = "http://10.0.2.2:3000/balance";
+  String url = "http://10.0.2.2:3000";
 
   Future<bool> hasPin({required String userId}) async {
     http.Response response = await http.post(
-      Uri.parse("$url/has-pin"),
+      Uri.parse("$url/balance/has-pin"),
       headers: {"Content-Type": "application/json"},
       body: json.encode(
         {
@@ -62,6 +62,51 @@ class BalanceService {
 
       if (response.statusCode == 200) {
         return (json.decode(response.body)["balance"] as int).toDouble();
+      }
+      throw Exception();
+    } on Exception {
+      return null;
+    }
+  }
+
+  Future getEmail() async {
+    try {
+      http.Response response = await http.post(
+          Uri.parse("$url/auth/find-email/?email=dandara@alura.com.br"),
+          headers: {"Content-Type": "application/json"});
+
+      if (response.statusCode == 200) {
+        return (json.decode(response.body));
+      }
+      throw Exception();
+    } on Exception {
+      return null;
+    }
+  }
+
+  Future createTransaction({
+    required String senderId,
+    required String receiverId,
+    required double amount,
+    required DateTime date,
+    required int type,
+  }) async {
+    try {
+      http.Response response = await http.post(
+        Uri.parse("$url/transaction/create-transaction"),
+        headers: {"Content-Type": "application/json"},
+        body: json.encode(
+          {
+            "senderId": senderId,
+            "receiverId": receiverId,
+            "amount": amount,
+            "date": date,
+            "type": type,
+          },
+        ),
+      );
+      if (response.statusCode == 200) {
+        return (json.decode(response.body));
       }
       throw Exception();
     } on Exception {
